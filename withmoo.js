424d5ce8d951f6499b7203df4c631309d748b303
Raphael <rylphs@gmail.com>
2017-09-01 17:55:38 -0300

    Atualizacao

--- withmoo.js
+++ withmoo.js
@@ -32,6 +32,24 @@ const formatFn = (arr)=>{
     return arr[0];
 }
 
+const args = (arr) => [arr[0]].concat(arr.splice(1).map((item)=>item[0]))
+
+const ungroup = (arr) => {
+    return [arr[1]].concat(
+        arr[3].reduce((flat, item) => {
+            flat.push(item[0], item[2]);
+            return flat;
+        }, [])
+    );
+}
+
+const joinOnFirst = (arr) => {
+    arr[0].value = arr[0].text = arr[0].text + flatten(arr.splice(1)).reduce(
+        (str, item) => !!item && item.value ? str + item.value : "", ""
+    );
+    return arr[0];
+}
+
 const n = function(n) { return (arr) => arr[n] };
 
 const ignore = (arr) => {};
@@ -40,21 +58,30 @@ const st = moo.states({
     main: {
         op: {match: /[\+\*\-\^\/]/},
         str_: {match: '"', push: "str"},
-        
         var: {match: /\$\{[0-9]+\}/},
-        rng_: {match: /\[/, push: "rng"},
-        lp: {match: /\(/, push: "main"},
+        lp: {match: /\(/},
         rp: {match: /\)/, pop: true},
-        fml: {match: /[\w]+[ \t]*\(/, push: "main"},
-        fmlp: {match: /\%[\w]+[ \t]*\(/, push: "main"},
+        fml: {match: /[a-zA-Z0-9]+[ \t]*\(/, push: "main"},
+        dyn: {match: /\%/, push: "dyn"},
+        //dynfml: {match: /\%[\w]+[ \t]*\(/, push: "main"},
+        //dynrng: {match: /\%(?:cell|row|col)/, push:"rngsel"},
         rng: {match: /(?:[a-zA-Z]+[0-9]+(?:\:[a-zA-Z]+[0-9]+)?|[a-zA-Z]+\:[a-zA-Z]+|[0-9]+\:[0-9]+)/},
         nb: {match: /[0-9]*\,?[0-9]+/},
         pspl: {match: /;/},
         spc: {match: /[\t ]+/}
     },
-    
-    rng:{
-        _rng: {match: /\]/, pop: true}
+    dyn: {
+        dynfml: {match: /[\w]+[ \t]*\(/, next: "main"},
+        dynrng: {match: /(?:cell|row|col)/, next:"rngsel"},
+    },
+    rngsel:{
+        rngop: {match: /[\>\<\+\-]/},
+        rngcount: {match: /[0-9]+/},
+        rngtp: {match:/(?:rows|cols)/},
+        rp: {match: /\)/, pop: true},
+        pspl: {match: /;/, pop:true},
+        spc: {match: /[\t ]+/, pop:true}
+       // _dynrng: {match: /(?:[ \t\;\)])/, pop: true}
     },
     str:{
         cnst: {match: /[^"]+/, lineBreaks: true},
@@ -67,21 +94,41 @@ var grammar = {
     Lexer: st,
     ParserRules: [
     {"name": "exp$ebnf$1", "symbols": []},
-    {"name": "exp$ebnf$1$subexpression$1", "symbols": [(st.has("op") ? {type: "op"} : op), "_", "member"]},
+    {"name": "exp$ebnf$1$subexpression$1", "symbols": ["op", "_", "member", "_"]},
     {"name": "exp$ebnf$1", "symbols": ["exp$ebnf$1", "exp$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "exp", "symbols": ["_", "member", "_", "exp$ebnf$1"], "postprocess": flatten},
-    {"name": "member", "symbols": [(st.has("nb") ? {type: "nb"} : nb)], "postprocess": id},
-    {"name": "member", "symbols": [(st.has("rng") ? {type: "rng"} : rng)], "postprocess": id},
-    {"name": "member", "symbols": [{"literal":"("}, "_", "exp", "_", {"literal":")"}], "postprocess": notNull},
-    {"name": "member$ebnf$1", "symbols": []},
-    {"name": "member$ebnf$1", "symbols": ["member$ebnf$1", "nxtArg"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "member", "symbols": ["fn", "_", "exp", "member$ebnf$1", {"literal":")"}], "postprocess": formatFn},
-    {"name": "fn", "symbols": [(st.has("fml") ? {type: "fml"} : fml)], "postprocess": id},
-    {"name": "fn", "symbols": [(st.has("fmlp") ? {type: "fmlp"} : fmlp)], "postprocess": id},
-    {"name": "nxtArg", "symbols": ["_", {"literal":";"}, "_", "exp"], "postprocess": n(3)},
+    {"name": "exp", "symbols": ["_", "member", "_", "exp$ebnf$1"], "postprocess": ungroup},
+    {"name": "member", "symbols": ["nb"], "postprocess": id},
+    {"name": "member", "symbols": ["parentesis"], "postprocess": id},
+    {"name": "member", "symbols": ["dynxp"]},
+    {"name": "member", "symbols": ["fmlxp"]},
+    {"name": "member", "symbols": ["rngxp"]},
+    {"name": "dynxp", "symbols": [{"literal":"%"}, "rngxp"]},
+    {"name": "dynxp", "symbols": [{"literal":"%"}, "fmlxp"]},
+    {"name": "parentesis", "symbols": [{"literal":"("}, "_", "exp", "_", {"literal":")"}]},
+    {"name": "rngxp", "symbols": ["rng"]},
+    {"name": "rngxp", "symbols": ["dynrngxp"]},
+    {"name": "dynrngxp$ebnf$1", "symbols": []},
+    {"name": "dynrngxp$ebnf$1$subexpression$1", "symbols": ["rngop", "rngcount", "rngtp"]},
+    {"name": "dynrngxp$ebnf$1", "symbols": ["dynrngxp$ebnf$1", "dynrngxp$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "dynrngxp", "symbols": ["dynrng", "dynrngxp$ebnf$1"]},
+    {"name": "fmlxp", "symbols": ["fml", "args", "_", {"literal":")"}]},
+    {"name": "fmlxp", "symbols": ["dynfml", "args", "_", {"literal":")"}]},
+    {"name": "args$ebnf$1", "symbols": []},
+    {"name": "args$ebnf$1$subexpression$1", "symbols": ["_", {"literal":";"}, "_", "exp"]},
+    {"name": "args$ebnf$1", "symbols": ["args$ebnf$1", "args$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "args", "symbols": ["exp", "args$ebnf$1"]},
+    {"name": "op", "symbols": [(st.has("op") ? {type: "op"} : op)], "postprocess": id},
+    {"name": "nb", "symbols": [(st.has("nb") ? {type: "nb"} : nb)], "postprocess": id},
+    {"name": "rng", "symbols": [(st.has("rng") ? {type: "rng"} : rng)]},
+    {"name": "dynrng", "symbols": [(st.has("dynrng") ? {type: "dynrng"} : dynrng)]},
+    {"name": "rngop", "symbols": [(st.has("rngop") ? {type: "rngop"} : rngop)]},
+    {"name": "rngcount", "symbols": [(st.has("rngcount") ? {type: "rngcount"} : rngcount)]},
+    {"name": "rngtp", "symbols": [(st.has("rngtp") ? {type: "rngtp"} : rngtp)]},
+    {"name": "fml", "symbols": [(st.has("fml") ? {type: "fml"} : fml)]},
+    {"name": "dynfml", "symbols": [(st.has("dynfml") ? {type: "dynfml"} : dynfml)]},
     {"name": "_$ebnf$1", "symbols": [(st.has("spc") ? {type: "spc"} : spc)], "postprocess": id},
     {"name": "_$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": ignore}
+    {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": id}
 ]
   , ParserStart: "exp"
 }
--- withmoo.ne
+++ withmoo.ne
@@ -28,6 +28,24 @@ const formatFn = (arr)=>{
     return arr[0];
 }
 
+const args = (arr) => [arr[0]].concat(arr.splice(1).map((item)=>item[0]))
+
+const ungroup = (arr) => {
+    return [arr[1]].concat(
+        arr[3].reduce((flat, item) => {
+            flat.push(item[0], item[2]);
+            return flat;
+        }, [])
+    );
+}
+
+const joinOnFirst = (arr) => {
+    arr[0].value = arr[0].text = arr[0].text + flatten(arr.splice(1)).reduce(
+        (str, item) => !!item && item.value ? str + item.value : "", ""
+    );
+    return arr[0];
+}
+
 const n = function(n) { return (arr) => arr[n] };
 
 const ignore = (arr) => {};
@@ -37,25 +55,29 @@ const st = moo.states({
         op: {match: /[\+\*\-\^\/]/},
         str_: {match: '"', push: "str"},
         var: {match: /\$\{[0-9]+\}/},
-        rng_: {match: /\[/, push: "rng"},
-        lp: {match: /\(/, push: "main"},
+        lp: {match: /\(/},
         rp: {match: /\)/, pop: true},
-        fml: {match: /[\w]+[ \t]*\(/, push: "main"},
-        dynfml: {match: /\%[\w]+[ \t]*\(/, push: "main"},
+        fml: {match: /[a-zA-Z0-9]+[ \t]*\(/, push: "main"},
+        dyn: {match: /\%/, push: "dyn"},
+        //dynfml: {match: /\%[\w]+[ \t]*\(/, push: "main"},
+        //dynrng: {match: /\%(?:cell|row|col)/, push:"rngsel"},
         rng: {match: /(?:[a-zA-Z]+[0-9]+(?:\:[a-zA-Z]+[0-9]+)?|[a-zA-Z]+\:[a-zA-Z]+|[0-9]+\:[0-9]+)/},
-        dynrng: {match: /\%(?:cell|row|col)[0-9]*/ push:}
         nb: {match: /[0-9]*\,?[0-9]+/},
         pspl: {match: /;/},
         spc: {match: /[\t ]+/}
     },
-    dynrng:{
-        shftleft{match: /\</ push: dynrng},
-        shftright{match: /\>/ push: dynrng},
-        addright:{match: /\+/},
-        addleft:{match: /\-/},
+    dyn: {
+        dynfml: {match: /[\w]+[ \t]*\(/, next: "main"},
+        dynrng: {match: /(?:cell|row|col)/, next:"rngsel"},
     },
-    rng:{
-        _rng: {match: /\]/, pop: true}
+    rngsel:{
+        rngop: {match: /[\>\<\+\-]/},
+        rngcount: {match: /[0-9]+/},
+        rngtp: {match:/(?:rows|cols)/},
+        rp: {match: /\)/, pop: true},
+        pspl: {match: /;/, pop:true},
+        spc: {match: /[\t ]+/, pop:true}
+       // _dynrng: {match: /(?:[ \t\;\)])/, pop: true}
     },
     str:{
         cnst: {match: /[^"]+/, lineBreaks: true},
@@ -68,9 +90,23 @@ const st = moo.states({
 
 @lexer st
 
-exp -> _ member _ (%op _ member):* {%flatten%}
-member -> %nb  {%id%} | %rng {%id%} | "(" _ exp _ ")" {%notNull%} | fn _ exp nxtArg:* ")" {%formatFn%}
-fn -> %fml {%id%} | %dynfml {%id%}
-nxtArg -> _ ";" _ exp {%n(3)%}
+exp -> _ member _ (op _ member _ ):*  {%ungroup%}
+member -> nb {%id%}  | parentesis {%id%} | dynxp | fmlxp | rngxp
+dynxp -> "%" rngxp | "%" fmlxp
+parentesis -> "(" _ exp _ ")"
+rngxp -> rng | dynrngxp
+dynrngxp -> dynrng (rngop rngcount rngtp):*
+fmlxp -> fml args _ ")" | dynfml args _ ")"
+args -> exp (_ ";" _ exp):* 
 
-_ -> %spc:? {%ignore%}
+op -> %op {%id%}
+nb -> %nb {%id%}
+rng -> %rng
+dynrng -> %dynrng
+rngop -> %rngop
+rngcount -> %rngcount
+rngtp -> %rngtp
+#_dynrng -> %_dynrng
+fml -> %fml
+dynfml -> %dynfml
+_ -> %spc:? {%id%}
